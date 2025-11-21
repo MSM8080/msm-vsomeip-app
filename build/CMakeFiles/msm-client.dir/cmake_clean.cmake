ec7a065a957db785fe188705d30c96a2c4c4dd30
MSM8080 <mohamedsaber605@gmail.com>
2025-11-21 20:31:43 +0200

    first push 21/11 8:31 PM

---
+++ build/CMakeFiles/msm-client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/msm-client.dir/src/msm-client.cpp.o"
+  "CMakeFiles/msm-client.dir/src/msm-client.cpp.o.d"
+  "msm-client"
+  "msm-client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/msm-client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/msm-service.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for msm-service.
+# This may be replaced when dependencies are built.
